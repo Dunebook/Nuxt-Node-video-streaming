6b30d4c81da542be0406be19a2ddc4a2462b9cf3
Deven Rathore <hyguyz@outlook.com>
2021-03-10 18:52:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.get('/video', (req, res) => {
 const Videos = require('./routes/Videos')
 app.use('/videos', Videos)
 
-.listen(process.env.PORT || 5000)
+app.listen(5000, () => {
+    console.log('Listening on port 5000!')
+}); 
 
-});
